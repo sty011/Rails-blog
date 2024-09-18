915dff7dc91b515503cd26105a29a2a684047880
sty011 <shmadad0@gmail.com>
2024-09-18 21:26:23 +0330

    Add the comment fields to the comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
-class Comment < ApplicationRecord
+class Comment
+
+  field :title, type: String
+  field :content, type: String
+
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
 end
