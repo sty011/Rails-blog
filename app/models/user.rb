c075bd7cfb1e04dfee777b4f2b2050a87d3b9642
sty011 <shmadad0@gmail.com>
2024-09-18 21:26:56 +0330

    Add the user fields to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
-class User < ApplicationRecord
+class User
+
+  field :username, type: String
+  field :password, type: String
+  field :email, type: String
+  field :first_name, type: String
+  field :last_name, type: String
+  field :bio, type: String
+  field :role, type: String
+  field :is_active, type: Boolean
+
+  index({ username: 1 }, { unique: true })
+  index({ email: 1 }, { unique: true })
 end
