d2571d396c87c5c5f95cd85ca22e5e7aea21ef93
sty011 <shmadad0@gmail.com>
2024-09-18 21:26:38 +0330

    Add the post fields to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
-class Post < ApplicationRecord
+class Post
+  
+  field :title, type: String
+  field :content, type: String
+  field :excerpt, type: String
+  field :slug, type: String
+
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :category, class_name: 'Category', foreign_key: 'category_id'
+
+  index({ slug: 1 }, { unique: true })
+
 end
